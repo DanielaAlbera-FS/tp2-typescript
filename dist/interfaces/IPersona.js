b7172dd8a8cb7c01bbafb082d1af46be30d5d34c
¨DanielaAlbera¨ <alberadaniela730@gmail.com>
2026-05-27 20:39:35 -0300

    Eliminar node_modules y dist

--- dist/interfaces/IPersona.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-/*Interfaz genérica que define la estructura base de cualquier persona
-T representa el tipo de fechaNacimiento, se define cuando la clase la implementa(no siempre)*/
-Object.defineProperty(exports, "__esModule", { value: true });
